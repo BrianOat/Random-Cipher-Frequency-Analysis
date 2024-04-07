6d6b19e791d79489b8997c9b741e84e8c10095f3
VD-25 <114820007+VD-25@users.noreply.github.com>
2024-04-07 19:25:31 -0400

    Update frequency_analysis.py

--- frequency_analysis.py
+++ frequency_analysis.py
@@ -14,7 +14,8 @@ for char in ciphertxt:
 
 # Most Frequent letters in the english language stored in a list.
 # 0 index indicates MOST frequent
-mostFreqEnglish= ["E", "T", "A", "O", "I", "N"]
+mostFreqEnglish= ["E", "T", "A", "O", "I", "N", "S", "R", "H", "D", "L", "U", "C", "M", "F", "Y", "W", "G", "P", "B", "V", "K", "X", "Q", "J", "Z"]
+#print(len(mostFreqEnglish))
 
 most=0
 i=0
@@ -24,15 +25,13 @@ sortedFreq = dict(sorted(freq.items(), key=lambda item: item[1], reverse=True))
 # Output the sorted dictionary
 print(sortedFreq)  # Output: {'a': 3, 'c': 2, 'b': 1}
 for key in sortedFreq:
-    if i==5:
-        break
     if i==0:
         print(f"The most frequent letter in the ciphertext is: {key}, therefore replacing it with the most frequent letter in the english language (e) gives us:")
-        replacedText = ciphertxt.replace(key, 'e')
+        replacedText = ciphertxt.replace(key, 'E')
         print(replacedText)
     else:
-        print(f"The next most frequent letter in the ciphertext was: {key}, therefore the replaced ciphertext is now:")
-        replacedText = ciphertxt.replace(key,mostFreqEnglish[i])
+        print(f"The next most frequent letter in the ciphertext was: {key}, therefore replacing it with " + mostFreqEnglish[i] + " is now:")
+        replacedText = replacedText.replace(key,mostFreqEnglish[i])
         print(replacedText)
     i+=1
 
